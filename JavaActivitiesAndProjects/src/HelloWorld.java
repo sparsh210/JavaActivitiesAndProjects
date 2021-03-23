a32acd9042630ffe98c617fc45d70f94db3cc3eb
spars <spars@10.0.0.33>
2021-03-23 13:50:50 -0400

    My first program! (added HelloWorld.java)

---
+++ JavaActivitiesAndProjects/src/HelloWorld.java
@@ -0,0 +1,9 @@
+//In this program I am writing code which will display "Hello World!"
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
